8a505f9d7d9c10ed44a38d69d32b227360303c97
Victor Hernandez <victormanu968@gmail.com>
2025-04-04 15:38:01 -0500

    add: prueba de enrutamiento para usuarios

--- src/main/java/com/softdev/api_gateway/config/Routes.java
+++ src/main/java/com/softdev/api_gateway/config/Routes.java
@@ -1,7 +1,25 @@
 package com.softdev.api_gateway.config;
 
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
 
 @Configuration
 public class Routes {
+
+    /**
+     * Configura la ruta para el microservicio de usuarios.
+     *
+     * @return una función de enrutamiento para el microservicio de usuarios.
+     */
+    @Bean
+    public RouterFunction<ServerResponse> userServiceRoute() {
+        return GatewayRouterFunctions.route("user_service")
+            .route(RequestPredicates.path("/api/user"), HandlerFunctions.http("http://localhost:8080"))
+            .build();
+    }
 }
