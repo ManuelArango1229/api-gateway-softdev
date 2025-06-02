e85dd12be37856eea411fdc4e31685aaee412520
Victor Hernandez <victormanu968@gmail.com>
2025-06-01 20:27:53 -0500

    fix: cambio en el gateway para uso de eureka

--- src/main/java/com/softdev/api_gateway/config/Routes.java
+++ src/main/java/com/softdev/api_gateway/config/Routes.java
@@ -1,51 +1,31 @@
 package com.softdev.api_gateway.config;
 
-import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
-import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.RequestPredicates;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerResponse;
 
+/**
+ * Configuración de rutas para los microservicios.
+ */
 @Configuration
 public class Routes {
 
     /**
-     * Configura la ruta para el microservicio de usuarios, que se encarga de autenticar.
-     *
-     * @return una función de enrutamiento para el microservicio de usuarios.
+     * Configura las rutas para los microservicios.
+     * @param builder
+     * @return
      */
     @Bean
-    public RouterFunction<ServerResponse> userAuthServiceRoute() {
-        return GatewayRouterFunctions.route("user_auth_service")
-            .route(RequestPredicates.path("/auth/**"), HandlerFunctions.http("http://localhost:8080"))
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route("user_service_auth", r -> r.path("/auth/**")
+                .uri("lb://user-service"))
+            .route("user_service", r -> r.path("/usuario/**")
+                .uri("lb://user-service"))
+            .route("product_service", r -> r.path("/producto/**")
+                .uri("lb://product-service"))
             .build();
     }
 
-    /**
-     * Configura la ruta para el microservicio de usuarios, que se encarga de las funciones CRUD de usuarios.
-     *
-     * @return una función de enrutamiento para el microservicio de usuarios.
-     */
-    @Bean
-    public RouterFunction<ServerResponse> userServiceRoute() {
-        return GatewayRouterFunctions.route("user_service")
-                .route(RequestPredicates.path("/usuario/**"), HandlerFunctions.http("http://localhost:8080"))
-                .build();
-    }
-
-    /**
-     * Configura la ruta para el microservicio de productos, que se encarga de las funciones CRUD de productos.
-     *
-     * @return una función de enrutamiento para el microservicio de productos.
-     */
-    @Bean
-    public RouterFunction<ServerResponse> productoServiceRoute() {
-        return GatewayRouterFunctions.route("product_service")
-                .route(RequestPredicates.path("/producto/**"), HandlerFunctions.http("http://localhost:8081"))
-                .build();
-    }
-
-    
 }
